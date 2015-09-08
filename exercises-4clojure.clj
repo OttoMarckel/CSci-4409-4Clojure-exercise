3f54bc473a4f34b09af818bc77d6480bdf087c12
Otto Marckel <marck018@morris.umn.edu>
2015-09-08 13:22:04 -0500

    Finished exercises

--- exercises-4clojure.clj
+++ exercises-4clojure.clj
@@ -43,11 +43,103 @@
 
 ;;; Problem 16: Hello World
 
-;
+;What I pasted
+
+format "Hello, %s!"
+
+;This takes what is after and makes one string
+
+;;; Problem 17: Sequences: Map
+
+;What I pasted
+
+'(6 7 8)
+
+;Add 5 to each one
+
+;;;Problem 18: Sequences: Filter
+
+;What I pasted
+
+'(6 7)
+
+;Return only those that are greater then 5
+
+;;;Problem 20: Penultimate element
+
+;What I pasted
+
+#(nth (reverse %) 1)
+
+;This counts backwards and takes the one that is 1 less then the last
+
+;;; Problem 21: Nth element
+
+;What I pasted
+
+(fn nthing [a b] (first (nthnext a b)))
+
+;takes a nd b to find nth element
+
+;;;Problem 23: Reverse a Sequence
+
+;What I pasted
+
+into '()
+
+;;;Problem 25: FInd the odd numbers
+
+;What I pasted
+
+filter odd?
+
+;This returns all odd numbers in the sequence
+
+;;;Problem 37: REgular Expressions
+
+;What I pasted:
+
+"ABC"
+
+;This returns all of the uppercase letters as requested
+
+;;;Problem 40: Interpose a Seq
+
+;What I pasted:
+
+(fn Interpose [a b] (drop-last (interleave b (repeat a))))
+
+;This function takes a and b to interleave the two
+
+;;;Problem 43: Reverse Interleave
+
+;What I pasted
+
+(fn Disassemble [a b] (apply map list(partition b a)))
+
+;Takes a and b where a is the list and b is the size of the new list
+
+;;;Problem 44: Rotate Sequence
+
+;What I pasted
+
+#(take (count %2) (drop (mod %1 (count %2)) (cycle %2)))
+
+;This takes a number and uses it to rotate the sequence around that spot
+
+;;;Problem 46: Flipping out
+
+;What I pasted
+
+(fn Flippers [a] (fn [c b] (a b c)))
+
+;This takes a and applies it to b and c in teverse order
 
 ;;;Problem 47: Contain Yourself
 
 ;What I pasted in the box
 
 4
+
+;The key
 
